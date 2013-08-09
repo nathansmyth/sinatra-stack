3141db17edc9022f250a251b153f6b8f24a4eabb
Nathan Smyth <nathan.smyth@reply-corp.com>
2013-08-09 15:22:17 -0700

    Add automotive makes in place of todos

--- public/js/automotive.js
+++ public/js/automotive.js
@@ -28,6 +28,22 @@ $(function(){
 
   });
 
+  // Make Model
+  // ----------
+
+  // **Make** model has MakeId, MakeName, and Models collection.
+  window.Make = Backbone.Model.extend({
+    idAttribute: "_id",
+
+    defaults: function() {
+      return {
+        MakeId: "0",
+        MakeName: "Automobile"
+      };
+    }
+
+  });
+
   // Todo Collection
   // ---------------
 
@@ -68,6 +84,90 @@ $(function(){
 
   // Create our global collection of **Todos**.
   window.Todos = new TodoList;
+
+
+  // Makes collection
+  window.MakeList = Backbone.Collection.extend({
+    model: Make,
+
+    url: '/api/makes'
+
+    // 
+  });
+
+  // Create our global collection of **Makes**.
+  window.Makes = new MakeList;
+
+  // Make Item View
+  // --------------
+
+  // The DOM element for a make item...
+  window.MakeView = Backbone.View.extend({
+    tagName: "li",
+    template: _.template($('#item-template-make').html()),
+    // The DOM events specific to an item.
+    events: {
+      "click .check"              : "toggleDone",
+      "dblclick div.todo-text"    : "edit",
+      "click span.todo-destroy"   : "clear",
+      "keypress .todo-input"      : "updateOnEnter"
+    },
+
+    // The TodoView listens for changes to its model, re-rendering.
+    initialize: function() {
+      this.model.bind('change', this.render, this);
+      this.model.bind('destroy', this.remove, this);
+    },
+
+    // Re-render the contents of the todo item.
+    render: function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      this.setText();
+      return this;
+    },
+
+    // To avoid XSS (not that it would be harmful in this particular app),
+    // we use `jQuery.text` to set the contents of the todo item.
+    setText: function() {
+      var text = this.model.get('text');
+      this.$('.todo-text').text(text);
+      this.input = this.$('.todo-input');
+      this.input.bind('blur', _.bind(this.close, this)).val(text);
+    },
+
+    // Toggle the `"done"` state of the model.
+    toggleDone: function() {
+      this.model.toggle();
+    },
+
+    // Switch this view into `"editing"` mode, displaying the input field.
+    edit: function() {
+      $(this.el).addClass("editing");
+      this.input.focus();
+    },
+
+    // Close the `"editing"` mode, saving changes to the todo.
+    close: function() {
+      this.model.save({text: this.input.val()});
+      $(this.el).removeClass("editing");
+    },
+
+    // If you hit `enter`, we're through editing the item.
+    updateOnEnter: function(e) {
+      if (e.keyCode == 13) this.close();
+    },
+
+    // Remove this view from the DOM.
+    remove: function() {
+      $(this.el).remove();
+    },
+
+    // Remove the item, destroy the model.
+    clear: function() {
+      this.model.destroy();
+    }
+  });
+
 
   // Todo Item View
   // --------------
@@ -155,7 +255,7 @@ $(function(){
     el: $("#automotive"),
 
     // Our template for the line of statistics at the bottom of the app.
-    statsTemplate: _.template($('#stats-template').html()),
+    // statsTemplate: _.template($('#stats-template').html()),
 
     // Delegated events for creating new items, and clearing completed ones.
     events: {
